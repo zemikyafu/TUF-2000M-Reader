4f0dc30023976f88300bc004a1776007a125f846
zemikyafu <zemikfcb@gmail.com>
2022-09-30 19:48:39 +0300

    Bearer Authorization security requirment added

--- TUF_2000M_API/Startup.cs
+++ TUF_2000M_API/Startup.cs
@@ -39,6 +39,30 @@ namespace TUF_2000M_API
                     Version = "v1",
                     Description= "API to read TUF_200M Data"
                 });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: ",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                     {
+                         {
+                               new OpenApiSecurityScheme
+                                 {
+                                     Reference = new OpenApiReference
+                                     {
+                                         Type = ReferenceType.SecurityScheme,
+                                         Id = "Bearer"
+                                     }
+                                 },
+                                 new string[] {}
+                         }
+                 });
+
             });
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
